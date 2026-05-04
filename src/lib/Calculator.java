cc89bfe3f71f9e8889455c36df8d6a68531bab8d
Jack <devjack050700@gmail.com>
2026-05-04 16:58:27 +0900

    0504: 챕터 6 패키지 예제 6-3

---
+++ src/lib/Calculator.java
@@ -0,0 +1,8 @@
+package lib;
+
+public abstract class Calculator {
+    public abstract int add();
+    public abstract int sub();
+    public abstract int mul();
+    public abstract double div();
+}
